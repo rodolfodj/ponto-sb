1dfec2385aa20fe0e33aa7e8072d76bdbd3cb709
Rodolfo Julio <rodolfodj@gmail.com>
2019-10-29 15:34:45 -0300

    Commit inicial

---
+++ src/main/java/com/rodolfo/pontosb/PontopringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rodolfo.pontosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontopringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontopringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodolfo/pontosb/PontopringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodolfo.pontosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontopringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
