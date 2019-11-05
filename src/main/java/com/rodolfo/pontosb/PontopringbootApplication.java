111e37f05f4a9f0f243871d4da891751cdf5f261
Rodolfo Julio <rodolfodj@gmail.com>
2019-11-05 12:30:26 -0300

    Desabilitando auth no Spring Security

--- src/main/java/com/rodolfo/pontosb/PontopringbootApplication.java
+++ src/main/java/com/rodolfo/pontosb/PontopringbootApplication.java
@@ -2,8 +2,9 @@ package com.rodolfo.pontosb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PontopringbootApplication {
 
 	public static void main(String[] args) {
