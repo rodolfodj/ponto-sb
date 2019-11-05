ccb89438a07fd22edcdd28c90afd139b841146b8
Rodolfo Julio <rodolfodj@gmail.com>
2019-11-05 20:03:57 -0300

    Cadastro pessoa física

--- src/main/resources/db/migration/postgresql/V1.1.sql
+++ src/main/resources/db/migration/postgresql/V1.1.sql
@@ -1,27 +1,27 @@
 
 
-CREATE TABLE public.empresa
+CREATE TABLE empresa
 (
     id bigint NOT NULL DEFAULT nextval('empresa_id_seq'::regclass),
-    cnpj character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    cnpj character varying(255)  NOT NULL,
     data_atualizacao timestamp without time zone NOT NULL,
     data_criacao timestamp without time zone NOT NULL,
-    razao_social character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    razao_social character varying(255)  NOT NULL,
     CONSTRAINT empresa_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE public.funcionario
 (
     id bigint NOT NULL DEFAULT nextval('funcionario_id_seq'::regclass),
-    cpf character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    cpf character varying(255)  NOT NULL,
     data_atualizacao timestamp without time zone NOT NULL,
     data_criacao timestamp without time zone NOT NULL,
-    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    nome character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    perfil character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(255)  NOT NULL,
+    nome character varying(255)  NOT NULL,
+    perfil character varying(255)  NOT NULL,
     qtd_horas_almoco real,
     qtd_horas_trabalho_dia real,
-    senha character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    senha character varying(255)  NOT NULL,
     valor_hora numeric(19,2),
     empresa_id bigint,
     CONSTRAINT funcionario_pkey PRIMARY KEY (id),
@@ -39,9 +39,9 @@ CREATE TABLE public.lancamento
     data timestamp without time zone NOT NULL,
     data_atualizacao timestamp without time zone NOT NULL,
     data_criacao timestamp without time zone NOT NULL,
-    descricao character varying(255) COLLATE pg_catalog."default",
-    localizacao character varying(255) COLLATE pg_catalog."default",
-    tipo character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    descricao character varying(255) ,
+    localizacao character varying(255) ,
+    tipo character varying(255)  NOT NULL,
     funcionario_id bigint,
     CONSTRAINT lancamento_pkey PRIMARY KEY (id),
     CONSTRAINT fk46i4k5vl8wah7feutye9kbpi4 FOREIGN KEY (funcionario_id)
